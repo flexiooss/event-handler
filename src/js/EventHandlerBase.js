c7031a69a5c050750209f4516cc719e91e41cd40
tomacht <thomas@flexio.fr>
2019-05-27 10:53:31 +0200

    add files from jshelper
    ref #1

---
+++ src/js/EventHandlerBase.js
@@ -0,0 +1,207 @@
+import {isBoolean,assert} from '@flexio-oss/assert'
+import {UID} from '../uid'
+import {Sequence} from '../Sequence'
+import {EventListenerParam} from './EventListenerParam'
+import {StringArray} from '@flexio-oss/extended-flex-types'
+
+const _isDispatching_ = Symbol('_isDispatching_')
+const _sequenceId_ = Symbol('_sequenceId_')
+
+/**
+ * @class
+ * @abstract
+ */
+export class EventHandlerBase {
+  constructor() {
+    /**
+     *
+     * @params {Map<(String|Symbol), Map<(String|Symbol), {
+        callback: EventHandlerBase~eventClb
+      } >>}
+     * @protected
+     */
+    this._listeners = new Map()
+
+    /**
+     * @callback EventHandlerBase~eventClb
+     * @param {Object} payload
+     * @param {(string|Symbol)} type
+     */
+
+    this._pendingPayload = new Map()
+    /**
+     *
+     * @params {Set<(string|Symbol)>}
+     * @protected
+     */
+    this._isHandled = new Set()
+    /**
+     *
+     * @params {Set<(String|Symbol)>}
+     * @protected
+     */
+    this._isPending = new Set()
+    this[_sequenceId_] = new Sequence(UID())
+
+    /**
+     * @property {boolean}
+     * @name EventHandlerBase#Symbol(_isDispatching_)
+     * @private
+     */
+    let _isDispatching = false
+    Object.defineProperty(this,
+      _isDispatching_,
+      {
+        enumerable: false,
+        configurable: false,
+        get: () => _isDispatching,
+        set: (v) => {
+          assert(isBoolean(v),
+            '_isDispatching argument should be a Boolean'
+          )
+          _isDispatching = v
+        }
+      })
+  }
+
+  /**
+   *
+   * @param {(String|Symbol)} event
+   * @param {Object} payload
+   */
+  dispatch(event, payload) {
+    if (this._listeners.has(event)) {
+      this._beforeDispatch(event, payload)
+      try {
+        this._listeners.get(event).forEach((v, k) => {
+          if (!this._isPending.has(k)) {
+            this._invokeCallback(event, k)
+          }
+        })
+      } finally {
+        this._stopDispatching(event)
+      }
+    }
+  }
+
+  /**
+   *
+   * @param {(string|Symbol)} event
+   * @protected
+   */
+  _ensureHaveListenersMap(event) {
+    if (!(this._listeners.has(event))) {
+      this._listeners.set(event, new Map())
+    }
+  }
+
+  /**
+   *
+   * @protected
+   * @param {(String|Symbol)} event of Listener
+   * @param {(String|Symbol)} token of Listener
+   */
+  _invokeCallback(event, token) {
+    this._isPending.add(token)
+    try {
+      this._listeners.get(event)
+        .get(token)
+        .callback(this._pendingPayload.get(event), event)
+    } finally {
+      this._isHandled.add(token)
+    }
+  }
+
+  /**
+   *
+   * @return {string}
+   */
+  nextID() {
+    return this[_sequenceId_].nextID()
+  }
+
+  /**
+   *
+   * @param {EventListenerParam} eventListenerParam
+   * @returns {(String|StringArray)} token
+   */
+  addEventListener(eventListenerParam) {
+    assert(eventListenerParam instanceof EventListenerParam,
+      'EventHandlerBase:addEventListener: ̀`eventListenerParam` argument assert be an instance of EventListenerParam'
+    )
+    const ids = new StringArray()
+    for (const event of eventListenerParam.events) {
+      this._ensureHaveListenersMap(event)
+      const id = this.nextID()
+
+      this._listeners.get(event)
+        .set(id, {
+          callback: eventListenerParam.callback
+        })
+      ids.push(id)
+    }
+
+    return ids.length > 1 ? ids : ids.first()
+  }
+
+  /**
+   *
+   * @param {(String|Symbol)} event of Listener
+   * @param {String} token
+   * @throws AssertionError
+   */
+  removeEventListener(event, token) {
+    if (this._listeners.has(event)) {
+      assert(this._listeners.get(event).has(token),
+        'EventHandlerBase:removeEventListener: ̀`id` argument not in _listeners : `%s`',
+        event
+      )
+      this._listeners.get(event).delete(token)
+    }
+  }
+
+  /**
+   *
+   * @param {(String|Symbol)} event of Listener
+   * @param {String} token
+   * @returns {boolean}
+   */
+  hasEventListener(event, token) {
+    return (this._listeners.has(event)) && (this._listeners.get(event).has(token))
+  }
+
+  /**
+   *
+   * @param {(String|Symbol)} event of Listener
+   * @param {Object} payload
+   * @private
+   */
+  _beforeDispatch(event, payload) {
+    this._listeners.get(event).forEach((v, k) => {
+      this._isHandled.delete(k)
+    })
+    this._pendingPayload.set(event, payload)
+    this[_isDispatching_] = true
+  }
+
+  /**
+   *
+   * @param {(string|Symbol)} event
+   * @protected
+   */
+  _stopDispatching(event) {
+    this._listeners.get(event).forEach((v, k) => {
+      this._isPending.delete(k)
+    })
+    this._pendingPayload.delete(event)
+    this[_isDispatching_] = false
+  }
+
+  /**
+   *
+   * @return {boolean}
+   */
+  isDispatching() {
+    return this[_isDispatching_]
+  }
+}
---
+++ src/js/EventListenerBuilder.js
@@ -0,0 +1,54 @@
+import {EventListenerParam} from './EventListenerParam'
+import {SymbolStringArray} from '@flexio-oss/extended-flex-types'
+
+export class EventListenerBuilder {
+  /**
+   *
+   * @param {SymbolStringArray} events
+   */
+  constructor(events) {
+    /**
+     *
+     * @type {SymbolStringArray}
+     * @protected
+     */
+    this._events = events
+    /**
+     *
+     * @type {EventHandlerBase~eventClb}
+     * @protected
+     */
+    this._callback = () => true
+  }
+
+  /**
+   *
+   * @param {...(String|Symbol)} events
+   * @return {EventListenerBuilder}
+   * @constructor
+   */
+  static listen(...events) {
+    return new this(new SymbolStringArray(...events))
+  }
+
+  /**
+   *
+   * @param {EventHandlerBase~eventClb} clb
+   * @return {EventListenerBuilder}
+   */
+  callback(clb) {
+    this._callback = clb
+    return this
+  }
+
+  /**
+   *
+   * @return {EventListenerParam}
+   */
+  build() {
+    return EventListenerParam.create(
+      this._events,
+      this._callback
+    )
+  }
+}
